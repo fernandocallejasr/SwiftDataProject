fa0e5e5f7c427b1c71d624218a1f189f18ec1c26
fernandocallejasr <ferd.callejas@gmail.com>
2024-08-04 11:10:48 -0600

    Initial Commit

---
+++ SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Fernando Callejas on 04/08/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
